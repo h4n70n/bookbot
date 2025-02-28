2e7f87f0e63fc4b705a19b4e5bd739068f3adc9c
h4n70n <colin.jenkel@outlook.com>
2025-02-28 00:22:22 -0800

    multi file refactor and args added

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 # print("hello world")
+from stats import get_num_words
+import sys
+
+#import system arguments
+def get_args():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1]
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = get_args()
     text = get_text(book_path)
-    word_count = len(text.split())
     letter_counts = count_chars(text)
+    print(f"{get_num_words(text)} words found in the document")
+    print()
 
     #Header for the output
     print("-----Begin letter count report-----")
 
-    print(f"There is a total word count of {word_count}")
-    print()
-
-    # Sort the dictionary by count
+    # Sort the dictionary by count, converts to a list of tuples
     sorted_letter_counts = sorted(letter_counts.items())
 
     #loop through the sorted list and print the key and value
@@ -23,18 +31,20 @@ def main():
     print("-----End letter count report-----")
  
 
-
+#Extract the text from the file
 def get_text(book_path):
     with open(book_path) as f:
         text = f.read()
     return text
     
     
-    
+#Produce dict with charecter counts
 def count_chars(text):
     letter_counts = {}
     for letter in text:
+        #Check if the charecter is a letter
         if letter.isalpha():
+            #convert the letter to lower case and count in dictionary
             letter_string = letter.lower()
             if letter_string in letter_counts:
                 letter_counts[letter_string] += 1
---
+++ stats.py
@@ -0,0 +1,6 @@
+def get_num_words(text):
+    word_count = len(text.split())
+    return word_count
+#word_count = len(text.split())
+#print(f"There is a total word count of {word_count}")
+#print()
